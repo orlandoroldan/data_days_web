9fbc2c9134a9e299ab59dbd3e0a5925f5f87ee11
Marc Gállego <mga2525@gmail.com>
2024-02-19 19:57:03 +0100

    create vite project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-gray-100 margin-0 p-4">
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+    </div>
+  );
+}

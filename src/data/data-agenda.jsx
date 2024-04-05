c75f809b06f08ff7dc8ab1b9ec024bc41b0a80d5
Marc Gállego <mga2525@gmail.com>
2024-04-06 01:06:02 +0200

    update some talks

--- src/data/data-agenda.jsx
+++ src/data/data-agenda.jsx
@@ -1,33 +1,28 @@
 export const data_agenda = [
     {
-        title: "XerradaXerradaXerradaXerradaXerradaXerradaXerrada",
+        title: "Machine Learning and Visual Effects",
+        author: "José Pérez Cano",
         level: "Advanced",
-        kind: "Crypto",
+        kind: "ML",
         small_description: "In the volatile realm of cryptocurrency, some small description to introduce the workshop.",
         description: "In the volatile realm of cryptocurrency, digital assets dance to the rhythm of blockchain technology, where every transaction is a node in a decentralized symphony. From the towering peaks of Bitcoin to the obscure altcoins lurking in the depths of the market, the landscape is a kaleidoscope of speculation and innovation. In the cryptoverse, bulls and bears engage in an eternal dance, as prices surge and plummet with the caprice of the wind. Investors ride the waves of volatility, seeking fortune amidst the chaos, while skeptics warn of bubbles waiting to burst.",
         date: "11 de maig",
-        time: "10h",
-        place: "A4202"
+        time: "TBC",
+        place: "TBC",
+        lang: 'EN',
     },
     {
-        title: "Xerrada Cryptocurrency 2",
+        title: "Desenvolupament Web Modern",
+        author: "Pau Fernández",
         level: "Beginner",
-        kind: "Crypto",
-        small_description: "In the volatile realm ofIn the volatile realm ofIn the volatile realm ofIn the volatile realm ofIn the volatile realm ofIn the volatile realm ofIn the volatile realm ofIn the volatile realm ofIn the volatile realm of cryptocurrency, some small description to introduce the workshop.",
-        description: "In the volatile realm of cryptocurrency, digital assets dance to the rhythm of blockchain technology, where every transaction is a node in a decentralized symphony. From the towering peaks of Bitcoin to the obscure altcoins lurking in the depths of the market, the landscape is a kaleidoscope of speculation and innovation. In the cryptoverse, bulls and bears engage in an eternal dance, as prices surge and plummet with the caprice of the wind. Investors ride the waves of volatility, seeking fortune amidst the chaos, while skeptics warn of bubbles waiting to burst.",
+        kind: "Web",
+        small_description: "Inicia't al món del desenvolupament web modern i fes el teu primer lloc web!",
+        description: "Una introducció al desenvolupament web modern, on aprendràs els conceptes bàsics de HTML, CSS i JavaScript. També parlarem de frameworks! Aquest taller està pensat per a aquells que vulguin començar a programar i no sàpiguen per on començar. No cal cap coneixement prèvi!",
         date: "11 de maig",
-        time: "10h",
-        place: "A4202"
+        time: "TBC",
+        place: "TBC",
+        lang: 'CA',
     },
-    {
-        title: "Xerrada Cryptocurrency 3",
-        level: "Intermediate",
-        kind: "Crypto",
-        small_description: "In the volatile realm of cryptocurrency, some small description to introduce the workshop.",
-        description: "In the volatile realm of cryptocurrency, digital assets dance to the rhythm of blockchain technology, where every transaction is a node in a decentralized symphony. From the towering peaks of Bitcoin to the obscure altcoins lurking in the depths of the market, the landscape is a kaleidoscope of speculation and innovation. In the cryptoverse, bulls and bears engage in an eternal dance, as prices surge and plummet with the caprice of the wind. Investors ride the waves of volatility, seeking fortune amidst the chaos, while skeptics warn of bubbles waiting to burst.",
-        date: "12 de maig",
-        time: "10h",
-        place: "A4202"
-    },
+
 ]
 

9e91c3c22c2b7db936490db0f1c9b59c10f79d73
Marc Gállego <mga2525@gmail.com>
2024-04-06 12:10:16 +0200

    afegir secció qui som

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,26 +12,34 @@ const Home = () => {
         <h2>Campus Nord, UPC</h2>
         <h2>11-12 de maig</h2>
         <br />
-        <p>Dels creadors de la Datathon FME, Data Days és una iniciativa innovadora organitzada per
-          l&apos;Associació d&apos;Estudiants de Dades (AED), destinada
-          a reunir entusiastes de la ciència i l&apos;enginyeria de les <b>dades</b> en una sèrie de conferències, tallers i activitats molt enriquidors.
-          No només pretén ser una oportunitat per aprendre de persones enteses del sector, sinó que també aporta
-          una perspectiva global del que suposa una carrera en la Ciència i Enginyeria de Dades.
+        <p>Dels creadors de la <a href='https://datathon.cat' target='_blank' rel='noopener'>Datathon FME</a>, Data Days Barcelona és una iniciativa innovadora,
+          destinada a reunir entusiastes de la ciència i l&apos;enginyeria de les <b>dades</b> en una sèrie de conferències, tallers i activitats molt enriquidors.
+          No només pretén ser una oportunitat per a fer comunitat i aprendre de persones enteses del sector, sinó que també aporta
+          una perspectiva global del que suposa una carrera en la ciència i enginyeria de dades.
           <br></br><br></br>
           L&apos;acte tindrà lloc al Campus Nord de la UPC els dies 11 i 12 de maig de 2024.
 
-          Aquest esdeveniment està enfocat a estudiants a molts nivells diferents, des de batxillerat o
-          estudiants de grau fins a estudiants de màster i recent graduats. Hi haurà xerrades i activitats adaptats
-          a diferents nivells per tal que tothom pugui participar i formar part de l&apos;experiència!
+          Hi haurà xerrades i activitats adaptats a diferents nivells per tal que tothom pugui participar i formar part de l&apos;experiència!
+          Podeu venir tant si sou estudiants universitaris, de batxillerat, recent graduats o simplement si teniu curiositat!
 
           <br></br><br></br>
 
-          Si tens qualsevol dubte sobre l&apos;esdeveniment, just a sota pots trobar la nostra pàgina de FAQs. Si encara no et resol
-          el dubte en qüestió, no dubtis a contactar-nos!
-
           Ens veiem el proper 11 de maig!
         </p>
-      </div>
+        <section className="fullwidth">
+          <div className="callout-with-image">
+            <div>
+              <img width="200px" src="aed.webp" alt="AED" />
+            </div>
+            <div>
+              <h3>Qui som?</h3>
+              <p>Som l&apos;associació estudiantil del <a href='https://dse.upc.edu/ca' target='_blank' rel='noopener'>grau en ciència i enginyeria de dades</a> de la <a href='https://upc.edu' target='_blank' rel='noopener'>UPC</a>. Treballem per a ajudar a millorar el grau, crear un vincle entre estudiants i alumni, organitzar activitats, compartir experiències de l&apos;alumnat i promoure l&apos;ecosistema de les dades català, entre moltes altres!</p>
+              <br />
+              <a href="https://aed.cat/" target="_blank" rel="noopener noreferrer">Més informació!</a>
+            </div>
+          </div>
+        </section >
+      </div >
       <div className="home-info-container">
         <div className="home-text">
           <div className='hidden-text'>

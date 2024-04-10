843fdfdc31fc60d3782cc6b68d545fca6d90c891
Laia Mogas <81615713+laiamp@users.noreply.github.com>
2024-04-10 19:33:00 +0200

    descripcio genomica

--- src/data/data-agenda.jsx
+++ src/data/data-agenda.jsx
@@ -51,7 +51,7 @@ export const data_agenda = [
         author: "Arcadi Navarro",
         level: "TBC",
         kind: "Conferència",
-        small_description: "Aviat...",
+        small_description: "Què és el Big Data en Genòmica Mèdica i què en podem fer? L'Arcadi Navarro, doctor en Biologia per la Universitat Autònoma de Barcelona, profesor d'invsetigació en ICREA i director de la Fundació Pasqual Maragall, ens donarà la resposta. ",
 
         description: "Aviat...",
         date: "11 de maig",
